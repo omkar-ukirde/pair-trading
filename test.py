d5d81fb0c53cc26180b13890ecab1cbaafc4d39d
omkar ukirde <10123675+omkar-ukirde@users.noreply.github.com>
2022-04-01 10:54:58 +0530

    Updated to print first 5 pairs

--- test.py
+++ test.py
@@ -1,17 +1,20 @@
 import pandas as pd
 import pdb
 
-watchlist = ['ACC','ALKEM','ASIANPAINT','BAJAJ-AUTO','BRITANNIA','CIPLA','COALINDIA','COLPAL','DABUR','DRREDDY','HCLTECH','HDFCBANK','HDFCLIFE','HEROMOTOCO','HINDUNILVR','ITC','IOC','INFY','KOTAKBANK','LT','MARICO','NTPC','NESTLEIND','PIDILITIND','POWERGRID','RELIANCE','TCS','TECHM','ULTRACEMCO','WIPRO']
+watchlist = ['HCLTECH','TECHM','SBILIFE','ASIANPAINT','WIPRO','ITC','INFY','TCS','RELIANCE','CIPLA','JSWSTEEL','HEROMOTOCO','NESTLEIND','HINDUNILVR','SHREECEM','BHARTIARTL','HDFCBANK','BRITANNIA','ICICIBANK','HDFCLIFE','TITAN','HINDALCO','BAJAJ-AUTO','DIVISLAB','SUNPHARMA','TATAMOTORS','UPL','TATASTEEL','BAJFINANCE','LT','SBIN','INDUSINDBK','TATACONSUM','COALINDIA','MARUTI','BPCL','ULTRACEMCO','HDFC','ADANIPORTS','DRREDDY','POWERGRID','BAJAJFINSV','AXISBANK','KOTAKBANK','IOC','M&M','ONGC','EICHERMOT','NTPC','GRASIM']
 data = pd.DataFrame()
 names = list()
 
 for name in watchlist:
-   df15 = pd.read_csv("C:\\Users\\omkin\\Downloads\\Algo Trading\\Session 7\\mywork5\\15mins\\"+name+".csv", parse_dates=['date'], index_col='date')
-   df15 = df15.rename(columns={'close':name})
-   data = pd.concat([data, df15[name]],axis = 1)
+   df = pd.read_csv("C:\\Users\\omkin\\OneDrive\\Documents\\Algo Trading\\"+name+".csv", parse_dates=['date'], index_col='date')
+   df = df.rename(columns={'close':name})
+   data = pd.concat([data, df[name]],axis = 1)
    names.append(name)
    data.columns = names
 
 corr_matrix = data.corr()
-corr_matrix.to_csv('heatmap.csv')   
-pdb.set_trace()
+corr_matrix = corr_matrix.stack().reset_index().rename(columns={'level_0':'Stock1','level_1':'Stock2', 0:'Correlation'})
+corr_matrix = corr_matrix[corr_matrix.Correlation != 1]
+corr_matrix = corr_matrix.sort_values(by=['Correlation'], ascending=False)
+corr_matrix = corr_matrix.drop_duplicates(subset='Correlation', keep='first')
+print(corr_matrix.head(10))
